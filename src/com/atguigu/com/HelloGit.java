54596af7d8456cff064bd939e4bd39992256985f
cscfnf <cscfnf@163.com>
2016-05-27 19:44:18 +0800

    修改 conflict

--- src/com/atguigu/com/HelloGit.java
+++ src/com/atguigu/com/HelloGit.java
@@ -2,7 +2,7 @@ package com.atguigu.com;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello33");
 		System.out.println("Hi");
 	}
 }
