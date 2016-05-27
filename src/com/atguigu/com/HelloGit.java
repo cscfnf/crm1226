5295de6b415ec4589c92daaef1f7d1c8bb026d18
cscfnf <cscfnf@163.com>
2016-05-27 19:41:29 +0800

    Update HelloGit.java

--- src/com/atguigu/com/HelloGit.java
+++ src/com/atguigu/com/HelloGit.java
@@ -2,7 +2,7 @@ package com.atguigu.com;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello111");
 		System.out.println("Hi");
 	}
 }
